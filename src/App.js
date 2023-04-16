e4ce729d7d23320c5ddf2a21043a128a6cf4cd8e
lavanmetta <lavanmetta111@gmail.com>
2023-04-16 23:48:26 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.scss'
-import Home from './components/Home/Home';
-import MovieDetails from './components/MovieDetails/MovieDetails';
-import PageNotFound from './components/PageNotFound/PageNotFound';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import "./App.scss";
+import Home from "./components/Home/Home";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <div className='main-container'>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path='/movie/:imdbID' element={<MovieDetails/>} />
-        <Route path='*' element={<PageNotFound/>} />
-      </Routes>
+      <div className="main">
+        <Header />
+        <div className="main-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:imdbID" element={<MovieDetails />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer/>
     </BrowserRouter>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,33 +4,49 @@ import Logo from "../../images/Logo.png";
 import User from "../../images/user.png";
 import "./Header.scss";
 import { useDispatch } from "react-redux";
-import { fetchAsyncMovies, fetchAsyncShows } from "../../features/movies/movieSlice";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../features/movies/movieSlice";
 
 function Header() {
-  const [term, setTerm] = useState("")
-  const dispatch = useDispatch()
+  const [term, setTerm] = useState("");
+  const dispatch = useDispatch();
   const submitHandler = (e) => {
     e.preventDefault();
-    if (term === "") return alert("Please enter search term")
-    dispatch(fetchAsyncMovies(term))
-    dispatch(fetchAsyncShows(term))
-    setTerm('')
-  }
+    if (term === "") return alert("Please enter search term");
+    dispatch(fetchAsyncMovies(term));
+    dispatch(fetchAsyncShows(term));
+    setTerm("");
+  };
   return (
     <div className="header">
       <Link to="/">
         <div className="logo">
           <img src={Logo} alt="logo" />
-          <h2>MoviesBro</h2>
+          <h2>Movies'Bro</h2>
         </div>
       </Link>
-      <div>
-      <form onSubmit={submitHandler}>
-      <input type="text" value={term} placeholder="search" onChange={(e) => {setTerm(e.target.value)}} />
-      <button type="submit">Search</button>
-      </form>
-      </div>
+
       <div className="user-image">
+        <div className="input-container">
+          <form onSubmit={submitHandler}>
+            <input
+              type="text"
+              value={term}
+              placeholder="Search Movies..."
+              onChange={(e) => {
+                setTerm(e.target.value);
+              }}
+            />
+            <div>
+              
+              <button type="submit">
+                <i className="ri-search-line"></i>
+              </button>
+            </div>
+          </form>
+        </div>
         <img src={User} alt="user" />
       </div>
     </div>
