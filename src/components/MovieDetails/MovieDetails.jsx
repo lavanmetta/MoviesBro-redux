c44209c807e7de90b8398e74c3bf61dc4e25108c
lavanmetta <lavanmetta111@gmail.com>
2023-04-18 20:04:04 +0530

    detals

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -11,7 +11,7 @@ function MovieDetails() {
   const { imdbID } = useParams();
   const dispatch = useDispatch();
   const data = useSelector(getSelectedMovieOrShow);
-  console.log(data)
+console.log(data.Ratings)
   useEffect(() => {
     dispatch(fetchAsyncMovieDetails(imdbID));
     return () => {
@@ -22,11 +22,56 @@ function MovieDetails() {
   return (
     <div>
       {Object.keys(data).length === 0 ? (
-        <div><h1>Loading....</h1></div>
-      ) : (
         <div>
-          <h1>{data.Title}</h1>
-          <p>{data.Plot}</p>
+          <h1>Loading....</h1>
+        </div>
+      ) : (
+        <div className="details-main-container">
+          <div className="img-container">
+            <div>
+              <img src={data.Poster} alt="img" />
+            </div>
+          </div>
+          <div className="details-container">
+            <div className="tp">
+              <h2>{data.Title}</h2>
+              <p>{data.Plot}</p>
+            </div>
+            <div className="line-one">
+              <p>
+                <i className="ri-team-line"></i> <span> {data.Actors}</span>
+              </p>
+              <p>
+                <i className="ri-flag-line"></i> <span> {data.Country}</span>
+              </p>
+              <p>
+                <i className="ri-movie-2-line"></i> <span> {data.Director}</span>
+              </p>
+            </div>
+
+            <div className="ratings">
+              <div className="one">
+                <div className="round">
+                  <h5>{data.imdbRating}</h5>
+                </div>
+                <p>IMDB</p>
+              </div>
+              {
+                
+                data.Ratings.map((each, index) => {
+                  return (
+                    <div className="one" key={index}>
+                      <div className="round">
+                        <h5>{each.Value}</h5>
+                      </div>
+                      <p>{each.Source.length > 10 ? each.Source.slice(0, 10) + '.....' : each.Source}</p>
+                    </div>
+                  );
+                })
+              }
+             
+            </div>
+          </div>
         </div>
       )}
     </div>
