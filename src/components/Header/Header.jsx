0603b731fa68c9f818946a496405332b651ca5bb
lavanmetta <lavanmetta111@gmail.com>
2023-04-18 17:30:44 +0530

    major css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,15 +11,29 @@ import {
 
 function Header() {
   const [term, setTerm] = useState("");
+  const [popUp, setPop] = useState(true);
   const dispatch = useDispatch();
+
+
+  const handlePop = () => {
+    setPop(prevState => !prevState);
+  }
+
+
   const submitHandler = (e) => {
     e.preventDefault();
     if (term === "") return alert("Please enter search term");
     dispatch(fetchAsyncMovies(term));
     dispatch(fetchAsyncShows(term));
-    
+
     setTerm("");
   };
+
+
+  
+ 
+ 
+
   return (
     <div className="header">
       <Link to="/">
@@ -32,21 +46,28 @@ function Header() {
       <div className="user-image">
         <div className="input-container">
           <form onSubmit={submitHandler}>
-            <input
-              type="text"
-              value={term}
-              placeholder="Search Movies..."
-              onChange={(e) => {
-                setTerm(e.target.value);
-              }}
-            />
-            <div>
-              
-              <button type="submit">
-                <i className="ri-search-line"></i>
-              </button>
+            <div className={popUp ? 'no-pop': 'pop'}>
+              <input
+                type="text"
+                value={term}
+                placeholder="Search Movies..."
+                onChange={(e) => {
+                  setTerm(e.target.value);
+                }}
+                
+              />
+              <div>
+                <button type="submit">
+                  <i className="ri-search-line"></i>
+                </button>
+              </div>
             </div>
           </form>
+          <div>
+            <button onClick={handlePop}>
+              <i className="ri-search-line"></i>
+            </button>
+          </div>
         </div>
         <img src={User} alt="user" />
       </div>
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import './MovieCard.scss'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./MovieCard.scss";
+import { Link } from "react-router-dom";
 function MovieCard(props) {
-  const {data} = props
-  console.log(data)
+  const { data } = props;
+  
   return (
     <div className="movie-card">
-    <Link to={`/movie/${data.imdbID}`}>
-      <div className='poster'>
-      <img src={data.Poster} alt="movie" />
-     
-      <p>{data.Title}</p>
-      <p >{data.Year}</p>
-     
-      
-      </div>
+      <Link to={`/movie/${data.imdbID}`}>
+        <div className="poster">
+          <img src={data.Poster} alt="movie" />
+          <div className="data ">
+          <p className='m-t'> {data.Title.length > 24 ? data.Title.slice(0, 20) + '.....' : data.Title}</p>
+          <p className='m-y data-m'>{data.Year}</p>
+        </div>
+        </div>
       </Link>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
