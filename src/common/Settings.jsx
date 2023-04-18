da4de2fea1430f6bcf6a18cd82616dbb6c643d14
lavanmetta <lavanmetta111@gmail.com>
2023-04-18 15:58:29 +0530

    css

--- src/common/Settings.jsx
+++ src/common/Settings.jsx
@@ -50,7 +50,7 @@ export const settings = {
   infinite: true,
   speed: 500,
   slidesToShow: 5,
-  slidesToScroll: 2,
+  slidesToScroll: 1,
   touchMove: true,
   centerMode: true,
   className: "center",
@@ -61,7 +61,7 @@ export const settings = {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         infinite: true,
         dots: true,
       },
@@ -70,7 +70,7 @@ export const settings = {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         initialSlide: 2,
       },
     },
